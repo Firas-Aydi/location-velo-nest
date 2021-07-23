a378cce529759cc4b891f4fb7a794b209df84587
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:27:10 +0100

    Create add-reparation.dto.ts

---
+++ src/reparation/dto/add-reparation.dto.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export class addReparationDto {
+    // matricule: string;
+    description: string;
+    date: Date;
+    prix: number;
+    idVelo: number;
+  }
