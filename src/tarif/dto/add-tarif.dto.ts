f19d8f764ee263835b2a4816f96b382ac5d8eb8f
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:26:00 +0100

    Create add-tarif.dto.ts

---
+++ src/tarif/dto/add-tarif.dto.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export class addTarifDto {
+    // typeClient: string;
+    // marqueVelo: string;
+    nombreHeure: number;
+    tarif: string;
+    idMar: string;
+    idTCl: string;
+  }
