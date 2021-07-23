8470f8ef7ac0562e214f8b517bc7def764ee8cdb
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:23:58 +0100

    Create compte.module.ts

---
+++ src/compte/compte.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompteController } from './compte.controller';
+import { CompteService } from './compte.service';
+import { Compte } from './entities/compte.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Compte])],
+  controllers: [CompteController],
+  providers: [CompteService],
+  exports: [CompteService],
+})
+export class CompteModule {}
