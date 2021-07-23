77c80a91ced7861a7ed7f8952753e0379fcaa74d
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:27:57 +0100

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Request, Response } from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use((req: Request, res: Response, next) => {
+    next();
+  });
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+  // app.setGlobalPrefix('api');
+  await app.listen(3000);
+}
+bootstrap();
