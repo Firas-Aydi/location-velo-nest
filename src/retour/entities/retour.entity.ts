a8b9d5e7d6f01eb1698b5fcc68ed64924fbc2f9b
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:26:16 +0100

    Create retour.entity.ts

---
+++ src/retour/entities/retour.entity.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+// import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// @Entity()
+// export class Retour {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+//   @Column()
+//   prenom: string;
+//   @Column()
+//   nom: string;
+//   @Column()
+//   HeureDepart: number;
+//   @Column()
+//   HeureArrivee: number;
+// }
