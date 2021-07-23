737ffbf4c45a3f59599e57cec312114a316d2660
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:22:48 +0100

    Create add-client.dto.ts

---
+++ src/client/dto/add-client.dto.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+// import { IsEmail, IsNotEmpty } from "class-validator";
+
+// export class addClientDto {
+//     prenom: string;
+//     nom: string;
+//     dateNaissance: Date;
+//     @IsEmail()
+//     email: string;
+//     telephone: number;
+//     type: string;
+//     @IsNotEmpty()
+//     password: string;
+//   }
+  
