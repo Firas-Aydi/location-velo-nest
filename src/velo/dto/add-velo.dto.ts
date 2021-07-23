de9056c926321476f29e6fed57b261f69ebee412
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:24:59 +0100

    Create add-velo.dto.ts

---
+++ src/velo/dto/add-velo.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export class addVeloDto {
+  // image: string;
+  // marque: string;
+  // modele: string;
+  // pouce: string;
+  matricule: string;
+  etat: string;
+  idMod: number;
+}
