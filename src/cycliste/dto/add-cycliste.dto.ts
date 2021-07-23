2a0abe9e6d23c76a53075a8a00eb1f4765f1eed3
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:42:55 +0100

    Create add-cycliste.dto.ts

---
+++ src/cycliste/dto/add-cycliste.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+
+export class addCyclisteDto {
+    prenom: string;
+    nom: string;
+    telephone: number;
+    reseauSoc: string;
+    taille: number;
+  }
+  
