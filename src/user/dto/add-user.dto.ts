390c78298ab51b28ebedeffab75a2744fdba530f
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:25:20 +0100

    Create add-user.dto.ts

---
+++ src/user/dto/add-user.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+// import { IsEmail, IsNotEmpty } from 'class-validator';
+
+// export class addUserDto {
+//     prenom: string;
+//     nom: string;
+//     @IsEmail()
+//     email: string;
+//     telephone: number;
+//     role: string;
+//     @IsNotEmpty()
+//     password: string;
+// }
