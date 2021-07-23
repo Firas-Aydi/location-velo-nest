9f49776830b320d98531d35adb467f9a588c9708
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:26:08 +0100

    Create retour.module.ts

---
+++ src/retour/retour.module.ts
@@ -0,0 +1,12 @@
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Retour } from './entities/retour.entity';
+// import { RetourController } from './retour.controller';
+// import { RetourService } from './retour.service';
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([Retour])],
+//   controllers: [RetourController],
+//   providers: [RetourService],
+// })
+// export class RetourModule {}
