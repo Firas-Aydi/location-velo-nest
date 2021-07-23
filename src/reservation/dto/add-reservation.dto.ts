515e9515c2333a42e5bf5a02e8a0c917049efb36
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:26:41 +0100

    Create add-reservation.dto.ts

---
+++ src/reservation/dto/add-reservation.dto.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+export class addReservationDto {
+  dateLocation: Date;
+  HeureDepart: number;
+  HeureArrivee: number;
+  // prenom: string;
+  // nom: string;
+  // telephone: number;
+  nbVelo:number;
+  montant: number;
+  avecGuide: boolean;
+  idCl: number;
+}
