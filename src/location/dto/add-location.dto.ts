e34945ce59decd362e0564253ee565184d7305e5
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:41:42 +0100

    Create add-location.dto.ts

---
+++ src/location/dto/add-location.dto.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+export class addLocationDto {
+  idRes: number;
+  dateLocation: Date;
+  HeureDepart: number;
+  HeureArrivee: number;
+  // prenom: string;
+  // nom: string;
+  cin: string;
+  cheque: string;
+  nbVelo:number;
+  circuit: string;
+  montant: number;
+  retour: boolean;
+  idCl: number;
+  idGui: number;
+}
