61161002d538c9b68d7eb181c6a0322b470958d9
Firas-Aydi <firasaydi0@gmail.com>
2021-07-23 14:43:21 +0100

    Create add-guide.dto.ts

---
+++ src/guide/dto/add-guide.dto.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export class addGuideDto {
+    prenom: string;
+    nom: string;
+    dateNaissance: Date;
+    email: string;
+    telephone: number;
+  }
+  
